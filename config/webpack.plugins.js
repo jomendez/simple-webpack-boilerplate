8d392a6169b95488e8eda1a587117e5cf38c45b3
Jose Mendez <jomendezdev@gmail.com>
2020-04-12 17:41:52 -0400

    removed unused dependencies

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -6,13 +6,10 @@ const fs = require('fs');
 
 const WebpackBar = require('webpackbar');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const WebappWebpackPlugin = require('webapp-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const RobotstxtPlugin = require('robotstxt-webpack-plugin');
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
 
 const config = require('./site.config');
 
@@ -36,12 +33,6 @@ const optimizeCss = new OptimizeCssAssetsPlugin({
   canPrint: true,
 });
 
-// Generate robots.txt
-const robots = new RobotstxtPlugin({
-  sitemap: `${config.site_url}/sitemap.xml`,
-  host: config.site_url,
-});
-
 // Clean webpack
 const clean = new CleanWebpackPlugin();
 
@@ -63,12 +54,6 @@ glob.sync('./src/**/*.html').map((dir) => {
     paths.push(filename);
   }
 
-});
-
-// Sitemap
-const sitemap = new SitemapPlugin(config.site_url, paths, {
-  priority: 1.0,
-  lastmodrealtime: true,
 });
 
 // Favicons
@@ -106,8 +91,6 @@ module.exports = [
   cssExtract,
   fs.existsSync(config.favicon) && favicons,
   config.env === 'production' && optimizeCss,
-  config.env === 'production' && robots,
-  config.env === 'production' && sitemap,
   webpackBar,
   config.env === 'development' && hmr,
 ].filter(Boolean);
